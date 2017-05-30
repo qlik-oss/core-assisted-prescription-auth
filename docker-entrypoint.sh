c4e32633f6d9b5c80b37c23532a779aa10c5b05f
Henrik Carlioth <hca@qlik.com>
2017-05-30 13:40:55 +0200

    default to Env. var. if both is set

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,10 +8,7 @@ file_env() {
 	local var="$1"
 	local fileVar="${var}_FILE"
 	local def="${2:-}"
-	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-		exit 1
-	fi
+	
 	local val="$def"
 	if [ "${!var:-}" ]; then
 		val="${!var}"
